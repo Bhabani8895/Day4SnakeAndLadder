7ed919e9145f50dad6345edd87b249778c72dd77
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2022-03-12 00:32:29 +0530

    player winning position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -17,25 +17,36 @@ namespace SnakeAndLadder
             const int LADDER = 2;
             const int SNAKE_BITE = 3;
             int position = 0;
-            Random random = new Random();
-            int dice = random.Next(1, 7);
-            int checkOption = random.Next(1, 4);
-            switch (checkOption)
+
+            while (position <= 100)
             {
-                case NO_PLAY:
-                    position = 0;
-                    break;
-                case LADDER:
-                    position = position + dice;
-                    break;
-                case SNAKE_BITE:
-                    position = position - dice;
-                    break;
-                default:
-                    position = 0;
-                    break;
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                int checkOption = random.Next(1, 4);
+                switch (checkOption)
+                {
+                    case NO_PLAY:
+                        position = 0;
+                        break;
+                    case LADDER:
+                        position = position + dice;
+                        break;
+                    case SNAKE_BITE:
+                        position = position - dice;
+                        break;
+                    default:
+                        position = 0;
+                        break;
+                }
+                if (position < 0)
+                {
+                    Console.WriteLine("Player restarts from 0");
+                }
+                else
+                {
+                    Console.WriteLine("Player stays in same postion");
+                }
             }
-            Console.WriteLine("postion of player is :" + position);
         }
     }
 }
